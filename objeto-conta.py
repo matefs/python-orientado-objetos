f0c7b161b0ddc9ab60e5e8c12e9c6fd058df0c7a
Mateus Schverz <mate_fs@hotmail.com>
2023-10-31 22:49:35 -0300

    feat: funcao extrato dentro da classe conta

--- objeto-conta.py
+++ objeto-conta.py
@@ -1,13 +1,15 @@
 
 class Conta: 
-
     def __init__(self, numero, titular, saldo, limite):
         print(f"Construindo o objeto {self}")
         self.numero = numero
         self.titular = titular
         self.saldo = saldo 
         self.limit = limite
+    def extrato(self):
+        print(f'Saldo {self.saldo} do titular {self.titular}')
 
 conta = Conta(123,"Mateus",100,1000000);
 print(conta)
 print(conta.numero)
+conta.extrato()
