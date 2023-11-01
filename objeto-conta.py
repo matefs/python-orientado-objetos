e7164b6c51df72104d5b6485a87f7a9a8819acfb
Mateus Schverz <mate_fs@hotmail.com>
2023-10-31 22:46:40 -0300

    Update objeto-conta.py

--- objeto-conta.py
+++ objeto-conta.py
@@ -1,7 +1,13 @@
 
 class Conta: 
 
-    def __init__ (self):
-        print(f"Contruindo objeto {self}")
+    def __init__(self, numero, titular, saldo, limite):
+        print(f"Construindo o objeto {self}")
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo 
+        self.limit = limite
 
-conta = Conta(); 
+conta = Conta(123,"Mateus",100,1000000);
+print(conta)
+print(conta.numero)
