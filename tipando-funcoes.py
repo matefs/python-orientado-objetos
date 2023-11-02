0b10974a51dfa148b81c21c1ba064f6c436db4bb
Mateus Schverz <mate_fs@hotmail.com>
2023-11-02 16:05:28 -0300

    refactor: union float int typed

--- tipando-funcoes.py
+++ tipando-funcoes.py
@@ -1,10 +1,14 @@
 '''
 Source: https://www.youtube.com/watch?v=PDqy1HoA3QM&ab_channel=NeuralNine
 '''
+from typing import Union 
 
+def some_calculation_integer(x: int , y: int ) -> int:
+    return (x+y)**2
 
-def some_calculation(x: int , y: int ) -> int:
+def some_calculation(x: Union[int,float] , y: Union[int,float] ) -> Union[int,float]:
     return ( x + y) **2 
 
 print(some_calculation(4,5))
+print(some_calculation(4.1,5))
 
