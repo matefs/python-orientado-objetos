a445e7cd3b74b3ade8e077a6003e9a52ffffa8d9
Mateus Schverz <mate_fs@hotmail.com>
2023-11-01 22:43:04 -0300

    feat: encapsulamento dos atributos

--- objeto-conta.py
+++ objeto-conta.py
@@ -2,21 +2,20 @@
 class Conta: 
     def __init__(self, numero, titular, saldo, limite):
         print(f"Construindo o objeto {self}")
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo 
-        self.limit = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo 
+        self.__limit = limite
   
     def extrato(self):
-        print(f'Saldo {self.saldo} do titular {self.titular}')
+        print(f'Saldo {self.__saldo} do titular {self.__titular}')
    
     def deposita(self,valor):
-        self.valor += valor
+        self.__valor += valor
 
     def saca(self,valor):
-        self.valor -= valor
+        self.__valor -= valor
     
 conta = Conta(123,"Mateus",100,1000000);
 print(conta)
-print(conta.numero)
 conta.extrato()
