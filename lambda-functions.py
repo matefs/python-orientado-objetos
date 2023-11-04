ad60a6ebaa4b28a605f96e238cb07cddf3898113
Mateus Schverz <mate_fs@hotmail.com>
2023-11-04 09:52:08 -0300

    Update lambda-functions.py

--- lambda-functions.py
+++ lambda-functions.py
@@ -1,8 +1,17 @@
 # Associate lambda function to a variable 
 
 IncreaseOneNumber = lambda x : x+1
-print(IncreaseOneNumber(1))
+print(f'Increased number {IncreaseOneNumber(1)}')
+
+# Map function 
+list(
+    map(lambda x : print(f'Map counter: {x}') , [1,2,3,4,5])
+)
 
 
 ## Lambda + map function 
-
+print(
+    list(
+        map(lambda x: x*2 , [100,200,300])
+        )
+    );
