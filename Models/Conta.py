7a07cb94971a2311272c7b7686239ee793381140
Mateus Schverz <mate_fs@hotmail.com>
2023-11-04 23:36:27 -0300

    Refactor: classe de conta foi separado dentro de models

---
+++ Models/Conta.py
@@ -0,0 +1,21 @@
+class Conta:
+    def __init__(self, numero, titular, saldo, limite):
+        print(f"Construindo o objeto {self}")
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo 
+        self.__limite = limite
+  
+    def extrato(self):
+        print(f'Saldo {self.__saldo} do titular {self.__titular}')
+   
+    def deposita(self, valor):
+        if valor>=1:
+            self.__saldo += valor
+        else:
+            return "Valor inválido"
+
+    def saca(self, valor):
+        if valor <= self.__saldo:
+            self.__saldo -= valor
+    
--- objeto-conta.py
+++ objeto-conta.py
@@ -1,21 +1,12 @@
+from Models.Conta import Conta
 
-class Conta: 
-    def __init__(self, numero, titular, saldo, limite):
-        print(f"Construindo o objeto {self}")
-        self.__numero = numero
-        self.__titular = titular
-        self.__saldo = saldo 
-        self.__limit = limite
-  
-    def extrato(self):
-        print(f'Saldo {self.__saldo} do titular {self.__titular}')
-   
-    def deposita(self,valor):
-        self.__valor += valor
+conta = Conta(123, "Mateus", 100, 1000000)
 
-    def saca(self,valor):
-        self.__valor -= valor
-    
-conta = Conta(123,"Mateus",100,1000000);
-print(conta)
+
+conta.deposita(10)
+conta.deposita(-10)
 conta.extrato()
+
+conta.saca(10000000)
+conta.extrato()
+
