03ec7e3baa6d141688501a6f3bb88029f3bd1956
Mateus Schverz <mate_fs@hotmail.com>
2023-11-07 23:55:34 -0300

    chore codigo banco e codigo_de_todos_bancos criados como metodos estaticos

--- Models/Conta.py
+++ Models/Conta.py
@@ -5,7 +5,6 @@ class Conta:
         self.__titular = titular
         self.__saldo = saldo 
         self.__limite = limite
-        self.__codigo_banco = '001'
   
     def extrato(self):
         print(f'Saldo {self.__saldo} do titular {self.__titular}')
@@ -47,8 +46,10 @@ class Conta:
     def limite(self,limite):
         self.__limite = limite
 
-    @property
-    def codigo_banco(self):
-        return self.__codigo_banco
-
+    @staticmethod
+    def codigo_banco():
+        return "001" 
     
+    @staticmethod
+    def codigo_de_todos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco':'237'}
